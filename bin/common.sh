04028ced0b0688987b767e6119a62d56eae52406
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-02-06 21:56:25 +0800

    Run update.sh

--- bin/common.sh
+++ bin/common.sh
@@ -2,7 +2,8 @@
 # shellcheck shell=bash
 
 channel_id=-1001664444944
-testing_group_id=-1001299514785
+testing_group_id=-1001299514785 # real testing group
+# testing_group_id=-1001155763792 # experimental testing group
 tgbot_home=$HOME/github-repo/mybot/telegram-bot-bash
 tgupload_dir=$tgbot_home/data-bot-bash
 
@@ -22,5 +23,5 @@ clean_log()
 clean_id()
 {
 	sed -i "/channel_id=$testing_group_id/d" "$HOME/bin/common.sh"
-	exit 1
+	exit 0
 }
