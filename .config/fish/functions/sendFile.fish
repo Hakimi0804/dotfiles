c307ea36c4458d98968bb4f6a42572d97efc2dda
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-04-02 22:04:51 +0800

    Update
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- .config/fish/functions/sendFile.fish
+++ .config/fish/functions/sendFile.fish
@@ -1,6 +1,13 @@
 function sendFile
-set -l chat_id $argv[1]
-set -l file $argv[2]
-source ~/.token.fish
-curl $API/sendDocument -F chat_id=$chat_id -F document=@$file
+    #argparse 'f/file=' -- $argv
+    #or return 1
+
+    set -l chat_id $argv[1]
+    set -l file $argv[2]
+    source ~/.token.fish
+
+    echo $_flag_file
+    set -q $_flag_file
+    #and curl $API/sendDocument -F chat_id=$chat_id -F document=@$_flag_file
+    curl $API/sendDocument -F chat_id=$chat_id -F document=@$file # Actually id but anyway
 end
