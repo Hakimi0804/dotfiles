15cb206426f072c8ea9dafbf7786f6d3a7f2ec33
Hakimi0804 <hakimifirdaus944@gmail.com>
2022-04-18 16:11:53 +0800

    lint through shfmt -s -w -i 4 $files
    Signed-off-by: Hakimi0804 <hakimifirdaus944@gmail.com>

--- filelist.sh
+++ filelist.sh
@@ -2,17 +2,16 @@
 ### File list to update
 #   -------------------
 
-
 extra_files=(
     "$HOME/.aliasrc"
     "$HOME/.fish_extras"
 )
 
 default=(
-	"$HOME/bin"
-	"$HOME/.bashrc"
-	"$HOME/.bashrc.d"
-	"$HOME/.nanorc"
-	"$HOME/.zshrc"
-	"$HOME/.config/fish"
+    "$HOME/bin"
+    "$HOME/.bashrc"
+    "$HOME/.bashrc.d"
+    "$HOME/.nanorc"
+    "$HOME/.zshrc"
+    "$HOME/.config/fish"
 )
--- update.sh
+++ update.sh
@@ -10,54 +10,52 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 reset='\033[0m'
 
-pr_green()
-{
-	echo -e "${green}$1${reset}"
+pr_green() {
+    echo -e "${green}$1${reset}"
 }
 
-pr_yellow()
-{
-	echo -e "${yellow}$1${reset}"
+pr_yellow() {
+    echo -e "${yellow}$1${reset}"
 }
 
 readonly HOME
 NEW_HOME=.
 pr_green "Starting to update default files..."
 for file in "${default[@]}"; do
-	pr_yellow " - Updating $file"
-	if [ -d "$file" ]; then
-		cp -r "$file"/* "$NEW_HOME/${file#"$HOME"/}" 2>/dev/null
-	else
-		cp -r "$file" "$NEW_HOME/${file#"$HOME"/}" 2>/dev/null
-	fi
-	# Make directory if it doesn't exist
-	if [ $? -eq 1 ]; then
-		mkdir -p "$NEW_HOME/${file#"$HOME"/}"
-		if [ -d "$file" ]; then
-			cp -r "$file"/* "$NEW_HOME/${file#"$HOME"/}"
-		else
-			cp -r "$file" "$NEW_HOME/${file#"$HOME"/}"
-		fi
-	fi
+    pr_yellow " - Updating $file"
+    if [ -d "$file" ]; then
+        cp -r "$file"/* "$NEW_HOME/${file#"$HOME"/}" 2>/dev/null
+    else
+        cp -r "$file" "$NEW_HOME/${file#"$HOME"/}" 2>/dev/null
+    fi
+    # Make directory if it doesn't exist
+    if [ $? -eq 1 ]; then
+        mkdir -p "$NEW_HOME/${file#"$HOME"/}"
+        if [ -d "$file" ]; then
+            cp -r "$file"/* "$NEW_HOME/${file#"$HOME"/}"
+        else
+            cp -r "$file" "$NEW_HOME/${file#"$HOME"/}"
+        fi
+    fi
 done
 
 echo
 
 pr_green "Starting to update extra files..."
 for file in "${extra_files[@]}"; do
-	pr_yellow " - Updating $file"
-	if [ -d "$file" ]; then
-		cp -r "$file"/* "$NEW_HOME/${file#"$HOME"/}" 2>/dev/null
-	else
-		cp -r "$file" "$NEW_HOME/${file#"$HOME"/}" 2>/dev/null
-	fi
-	# Make directory if it doesn't exist
-	if [ $? -eq 1 ]; then
-		mkdir -p "$NEW_HOME/${file#"$HOME"/}"
-		if [ -d "$file" ]; then
-			cp -r "$file"/* "$NEW_HOME/${file#"$HOME"/}"
-		else
-			cp -r "$file" "$NEW_HOME/${file#"$HOME"/}"
-		fi
-	fi
+    pr_yellow " - Updating $file"
+    if [ -d "$file" ]; then
+        cp -r "$file"/* "$NEW_HOME/${file#"$HOME"/}" 2>/dev/null
+    else
+        cp -r "$file" "$NEW_HOME/${file#"$HOME"/}" 2>/dev/null
+    fi
+    # Make directory if it doesn't exist
+    if [ $? -eq 1 ]; then
+        mkdir -p "$NEW_HOME/${file#"$HOME"/}"
+        if [ -d "$file" ]; then
+            cp -r "$file"/* "$NEW_HOME/${file#"$HOME"/}"
+        else
+            cp -r "$file" "$NEW_HOME/${file#"$HOME"/}"
+        fi
+    fi
 done
